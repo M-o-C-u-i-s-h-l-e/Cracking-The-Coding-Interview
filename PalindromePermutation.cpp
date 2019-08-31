72784016e5ffaa94b3010b76c4d58c05178c2673
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-08-31 14:49:25 +0530

    Update PalindromePermutation.cpp

--- PalindromePermutation.cpp
+++ PalindromePermutation.cpp
@@ -41,3 +41,25 @@ int main(void) {
         (++v[str[i] - 'a'] & 1) ? odd++ : odd--;
     (odd > 1) ? cout << "False\n" : cout << "True\n";
 }
+
+
+
+
+
+// Another Method
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string str;
+    cin >> str;
+    long long odd = 0;
+    for (int i = 0; i < str.size(); i++)
+        odd ^= (1 << (str[i] - 'a'));
+    (__builtin_popcount(odd) > 1) ? cout << "False\n" : cout << "True\n";
+}
