f383d6b19ad6676a7ce1e7699fd45215003b88a4
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-08-31 23:36:41 +0530

    Update Partition.cpp

--- 2. Linked Lists/Partition.cpp
+++ 2. Linked Lists/Partition.cpp
@@ -30,3 +30,27 @@ node *partition(node *head, int x) {
 	beforeEnd->next = afterStart;
 	return beforeStart;
 }
+
+
+
+
+
+// Another Method
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+node *partition(node *n, int x) {
+	node *head = n, *tail = n;
+	while (n != NULL) {
+		node *next = n->next;
+		if (n->data < x) {
+			n->next = head;
+			head = n;
+		} else {
+			tail->next = n;
+			tail = n;
+		}
+		n = next;
+	}
+	tail->next = NULL;
+	return head;
+}
