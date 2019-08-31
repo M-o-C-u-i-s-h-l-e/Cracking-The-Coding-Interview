4d293cd44104f7fe663f6be17627a3a42959c8a4
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-08-31 22:30:20 +0530

    Update and rename sol.cpp to RemoveDups.cpp

---
+++ 2. Linked Lists/RemoveDups.cpp
@@ -0,0 +1,13 @@
+void deleteDups(node *head) {
+	set<int> s;
+	node *prev = NULL;
+	while (head != NULL) {
+		if (s.count(head.data)) {
+			prev.next = head.next;
+		} else {
+			s.insert(head.data);
+			prev = head;
+		}
+		head = head.next;
+	}
+}
