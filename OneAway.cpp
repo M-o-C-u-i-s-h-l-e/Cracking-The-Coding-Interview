741dfd6874b05d1ca9d2c00ec7dd5c8fa548f00b
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-08-31 18:40:27 +0530

    Update OneAway.cpp

--- OneAway.cpp
+++ OneAway.cpp
@@ -47,3 +47,44 @@ int main(void) {
 	cin >> s1 >> s2;
 	(oneEditAway(s1, s2)) ? cout << "True\n" : cout << "False\n";
 }
+
+
+
+
+
+// Another Method
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+bool oneEditAway(string first, string second) {
+	if (abs(int(first.size() - second.size())) > 1)
+		return false;
+	string s1 = (first.size() > second.size()) ? second : first;
+	string s2 = (first.size() > second.size()) ? first : second;
+	int idx1 = 0, idx2 = 0;
+	bool foundDiff = false;
+	while (idx1 < s1.size() && idx2 < s2.size()) {
+		if (s1[idx1] != s2[idx2]) {
+			if (foundDiff)
+				return false;
+			foundDiff = true;
+			if (s1.size() == s2.size())
+				idx1++;
+		} else {
+			idx1++;
+		}
+		idx2++;
+	}
+	return true;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	string s1, s2;
+	cin >> s1 >> s2;
+	(oneEditAway(s1, s2)) ? cout << "True\n" : cout << "False\n";
+}
