6209054a8ffc275460a2ce137e7bb7ce47bd018f
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-08-31 22:54:19 +0530

    Update ReturnKthToLast.cpp

--- 2. Linked Lists/ReturnKthToLast.cpp
+++ 2. Linked Lists/ReturnKthToLast.cpp
@@ -13,3 +13,24 @@ node *nthToLast(node *head, int k, int &i) {
 node *nthToLast(node *head, int k) {
 	return nthToLast(head, k, 0);
 }
+
+
+
+
+
+// Another Method
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+node *nthToLast(node *head, int k) {
+	node *p1 = head, *p2 = head;
+	for (int i = 0; i < k; i++) {
+		if (p1 == NULL)
+			return NULL;
+		p1 = p1->next;
+	}
+	while (p1 != NULL) {
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	return p2;
+}
