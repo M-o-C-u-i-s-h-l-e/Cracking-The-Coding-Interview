8371f3c295ddad4ef0090a7a8f4f86defad694b9
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-09-01 09:09:58 +0530

    Update Palindrome.cpp

--- 2. Linked Lists/Palindrome.cpp
+++ 2. Linked Lists/Palindrome.cpp
@@ -51,3 +51,48 @@ bool palindrome(node *n) {
 	}
 	return true;
 }
+
+
+
+
+
+// Another Method
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Result {
+public:
+	node *n = NULL;
+	bool result = true;
+	Result(node *n, bool result) {
+		this->n = n;
+		this->result = result;
+	}
+};
+
+int length(node *n) {
+	int len = 0;
+	while (n != NULL) {
+		n = n->next;
+		len++;
+	}
+	return len;
+}
+
+Result *isPalindrome(node *n, int len) {
+	if (n == NULL || len <= 0)
+		return new Result(n, true);
+	if (len == 1)
+		return new Result(n->next, true);
+	Result *res = isPalindrome(n->next, len - 2);
+	if (!res.result || res.node == NULL)
+		return res;
+	res->result = (n->data == res->n->data);
+	res->n = res->n->next;
+	return res;
+}
+
+bool palindrome(node *n) {
+	int len = length(n);
+	Result *res = isPalindrome(n, len);
+	return res.result;
+}
